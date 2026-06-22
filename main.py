38b8e70920bd08184656e499d416d4756b6698bd
Pardeep-Singh-98 <email.singhpardeep@gmail.com>
2026-06-22 17:46:06 +0530

    Update ChatAi Code

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import os
 
 from dotenv import load_dotenv
-<<<<<<< HEAD
-
-load_dotenv()
-=======
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 load_dotenv(override=True)
 api_key = os.getenv("OPENAI_API_KEY")
->>>>>>> project/hello-world
 
 
 def main():
     print("Hello from langchain-course!")
-<<<<<<< HEAD
-   
-=======
+    # print("api key = " + api_key)
 
     information = """
         Elon Reeve Musk (/ˈiːlɒn/ ⓘ EE-lon; born June 28, 1971) is a businessman and former public official who is the CEO and largest shareholder of Tesla and SpaceX. Musk has been the wealthiest person in the world since 2025, and became the first and only trillionaire in terms of US dollars in 2026;[c] as of June 2026, Forbes estimates his net worth to be US$1.2 trillion.
@@ -40,11 +33,9 @@ def main():
         model="gemini-2.5-flash",
     )
     chain = summary_prompt_template | llm
-    # print(chain)
 
     response = chain.invoke(input={"information": information})
     print(response.content)
->>>>>>> project/hello-world
 
 
 if __name__ == "__main__":
